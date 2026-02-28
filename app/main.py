c7571518a6bc887fb22434d62b62c7a8da91bda1
Professional_Mario <mani1756067@gmail.com>
2026-02-28 22:56:21 +0530

    database connection established

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI,Request
 from pydantic import BaseModel
-# from fastapi import FastAPI, Request
+import sqlite3
 from fastapi.templating import Jinja2Templates
+import os 
 
 app= FastAPI()
 
-templates = Jinja2Templates(directory="../templates")
+templates = Jinja2Templates(directory="templates")
 
 class QueryRequest(BaseModel):
     question: str
@@ -14,7 +15,21 @@ class QueryRequest(BaseModel):
 def root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
-@app.post('/query')
+@app.post("/query")
 def receive_query(request: QueryRequest):
-    print(request.question)
-    return {"Received question": request.question}
+    try:
+
+        conn = sqlite3.connect("Chinook_Sqlite.sqlite")
+        cursor = conn.cursor()
+        
+
+        cursor.execute(request.question)
+        rows = cursor.fetchall()
+
+        conn.commit()
+        conn.close()
+
+        return {"result": rows}
+
+    except Exception as e:
+        return {"error": str(e)}
