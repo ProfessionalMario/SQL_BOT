31c1e7efcbfea19a5fc8c17e53843781034c562b
Professional_Mario <mani1756067@gmail.com>
2026-02-28 21:58:54 +0530

    Web UI Done

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
-print("Run")
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
+# from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app= FastAPI()
+
+templates = Jinja2Templates(directory="../templates")
+
+class QueryRequest(BaseModel):
+    question: str
+
+@app.get("/")
+def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.post('/query')
+def receive_query(request: QueryRequest):
+    print(request.question)
+    return {"Received question": request.question}
